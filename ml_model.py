c2c2a76ed51b0ce2c918022e33e74d58c8618385
dilipcm <cmdilip2@gmail.com>
2023-09-09 15:53:44 +0530

    git conflict example

--- ml_model.py
+++ ml_model.py
@@ -4,12 +4,12 @@ print("adding all ml related code here")
 
 
 def model1():
-    print("adding code for ML Model1")
+    print("Modifiying code for ML Model1")
 
 def model2():
     print("adding code for ML Model2")
 
 def model3():
-    print("adding code for ML Model3")
+    print("Modifiying code for ML Model3")
 
 print("model 2 is performing best")
